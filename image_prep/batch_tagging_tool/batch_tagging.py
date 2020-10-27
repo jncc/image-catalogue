e09622f458d9b13b40a4bbd194bd0a54db8daa76
Graeme Duncan <Graeme.Duncan@jncc.gov.uk>
2020-10-27 08:49:13 +0000

    second batch update
    missed files from previous round

--- image_prep/batch_tagging_tool/batch_tagging.py
+++ image_prep/batch_tagging_tool/batch_tagging.py
@@ -26,8 +26,11 @@ import datetime
 import csv
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 from pathlib import Path, PurePath
 import shutil
+
+print("Loaded modules")
 
 def subprocess_cmd(command):
     """Execute sub-processing commands to the command line tool ExifTool.
@@ -35,7 +38,7 @@ def subprocess_cmd(command):
     This function uses Popen to simultaneously execute multiple executions and passes the outputs into subsequent
     sub-processes through a pipe"""
     print(command)
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, encoding="utf8")
+    process = subprocess.call(command, stdout=subprocess.PIPE, shell=True, encoding="utf8")
 
 def rewritePath(fp, wd):
     imageName = os.path.basename(fp)
@@ -48,18 +51,32 @@ if __name__ == '__main__':
     #   1b) Setting a working directory for file access - this must be set to the final file path destination for the output
     #       log files. Please not that this should also be where the image proforma file is saved.
 
-    root = tk.Tk()
-    root.withdraw()
-
-    wd = filedialog.askdirectory(title = "Select output directory")
+    #root = tk.Tk()
+    #root.withdraw()
+    print("In main loop")
+    #wd = filedialog.askdirectory(title = "Select output directory")
+    wd = input("Paste output directory here")
     os.chdir(wd)
+    print(f"Changed working directory to: {wd}")
 
     exiftoolPath = str(Path(__file__).parents[2] / 'exiftool' / 'exiftool.exe')
 
     #       Read in metadata from .xlsx format files as Pandas (pd) DataFrames
 
-    proformaLoc = filedialog.askopenfilename(title = "Select image proforma metadata spreadsheet for survey in question")
+    #proformaLoc = filedialog.askopenfilename(title = "Select image proforma metadata spreadsheet for survey in question")
+    proformaLoc = input("Paste location of image proforma metadata spreadsheet here:")
     imageProforma = pd.read_excel(proformaLoc, 'Image_Proforma')
+    
+    #copyfiles = messagebox.askyesno("Copy files or edit insitu","Would you like to make copies of the input image files?\r\n(Select 'No' only if you're sure you're already working on copies)")
+    #        Double check!!
+    #if not copyfiles:
+    #    copyfiles = not messagebox.askyesno("Copy files or edit insitu","Are you sure you want to work directly on the files defined in the spreadsheet?")
+
+    copyFilesResponse = input("Do you want to [copy] files or work on [originals]?")
+    if copyFilesResponse.lower() == "originals":
+        copyfiles = False
+    else:
+        copyfiles = True
 
     #       Set preliminary ExifTool args
 
@@ -73,18 +90,25 @@ if __name__ == '__main__':
         arg = '-' + row['Field'] + '=' + str(row['Value'])
         initialExifToolArgs.append(arg)
 
-    #   1c) Create new column in pd dataframe to store new filepath
-    # TODO - identify common root of all images, and create output folders as necessary.
-    #commonRoot = os.path.commonpath([imageProforma['FilePath']])
+    if copyfiles:
+        #   1c) If user has selected to copy files (the default), create new column in pd dataframe to store new filepath
+        # TODO - identify common root of all images, and create output folders as necessary.
+        #commonRoot = os.path.commonpath([imageProforma['FilePath']])
 
-    imageProforma['NewPath'] = imageProforma['FilePath'].apply(lambda fp: rewritePath(fp, wd))
+        imageProforma['NewPath'] = imageProforma['FilePath'].apply(lambda fp: rewritePath(fp, wd))
 
-    #   1d) Copy across files to new directory
-    print("Copying files across!")
-    for index, row in imageProforma.iterrows():
-        oldPath = row['FilePath']
-        print(f'    copying {oldPath}')
-        shutil.copy2(oldPath, row['NewPath'])
+        #   1d) Copy across files to new directory
+        print("Copying files across!")
+        for index, row in imageProforma.iterrows():
+            oldPath = row['FilePath']
+            print(f'    copying {oldPath}')
+            shutil.copy2(oldPath, row['NewPath'])
+    
+    else:
+        #   1x) If user has selected to not copy files, set NewPath as FilePath to continue as normal
+        print("User selected to not copy files, working on originals files defined in spreadsheet")
+        imageProforma['NewPath'] = imageProforma['FilePath']
+        
 
     #   2) Identify list features within image proforma, change data type, and remove identifier flag from column name
 
@@ -116,7 +140,12 @@ if __name__ == '__main__':
                             argStr = '-{}={}'.format(colName, cellVal)
                             fileExifToolArgs.append(argStr)
                 fileExifToolArgs.append(filePath)
-                subprocess_cmd(fileExifToolArgs)
+
+                ## running subprocess.call instead rather than contained function to wait for each exiftool call individually and stop runaway processes opening!
+                #subprocess_cmd(fileExifToolArgs)
+                print(fileExifToolArgs)
+                subprocess.Popen(fileExifToolArgs, encoding="utf8")
+                time.sleep(5)
                 # print(fileExifToolArgs)
             except Exception as e:
                 print(str(e) + '{}'.format(filePath))
